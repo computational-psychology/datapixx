d702325ef084379c58e8ae32f03a5fad2f188c95
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2017-12-19 18:49:19 +0100

    edits example.py to test connection with datapixx

--- example/example.py
+++ example/example.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 19 18:07:39 2017
+Example script to test connection with DATAPixx      
 
-@author: guille
+@author: Guillermo Aguilar, Dec 2017
 """
 
+import time
 import sys
 sys.path.insert(0, "../build/dist-packages")
 
-import datapixx
+import datapixx as dpx
+
+# Open datapixx.
+datapixx = dpx.open()
+
+# set videomode: Concatenate Red and Green into a 16 bit luminance
+# channel.
+datapixx.setVidMode(dpx.DPREG_VID_CTRL_MODE_M16)
+
+# Demonstrate successful initialization with button blinks
+datapixx.blink(dpx.BWHITE)
+
+datapixx.blink(dpx.BWHITE | dpx.BBLUE | dpx.BGREEN
+            | dpx.BYELLOW | dpx.BRED)
+                        
+datapixx.blink(dpx.BBLUE | dpx.BGREEN | dpx.BYELLOW | dpx.BRED)
+
+
+# closes
+datapixx.close()
